3c5a618937dcd31d18716bbc0f824d6c3d4c11e7
Keyvan Kabirnia <keyvan.business22@gmail.com>
2025-07-28 17:53:07 +0330

    Register DbContext and provide options - add DbConfiguration class for loading db settings with option pattern

---
+++ src/Tms.Domain/Configurations/DbConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tms.Domain.Configurations;
+public class DbConfiguration
+{
+    public string Server { get; set; } = string.Empty;
+    public string Port { get; set; } = string.Empty;
+    public string DbName { get; set; } = string.Empty;
+    public string UserId { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool TrustServerCertificate { get; set; }
+    public bool MultipleActiveResultSets { get; set; }
+
+    public string ConnectionString =>
+        $"Server={Server},{Port};Database={DbName};User Id={UserId};Password={Password}!;TrustServerCertificate={TrustServerCertificate};MultipleActiveResultSets={MultipleActiveResultSets}";
+}
